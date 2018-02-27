903e87704f1b1810d572a3e3a0a9c698c639234c
wsvincent <will@wsvincent.com>
2018-02-27 14:34:31 -0500

    add STATIC_ROOT setting, base.css, and base.js files

--- djangox/settings.py
+++ djangox/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Bootstrap Crispy-Forms config
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
