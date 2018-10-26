122ef9672b685824bc9a00a421f45e02885a8cef
wsvincent <will@wsvincent.com>
2018-10-26 16:12:18 -0400

    add staticfiles_dirs

--- djangox_project/settings.py
+++ djangox_project/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+] 
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
