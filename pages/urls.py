ae0598cb1a364e8222f71d57526678e924eb4cb3
willv <wsv@wsv.local>
2018-11-02 12:06:10 -0400

    update pages/urls.py views import

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
-    path('about/', views.AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
