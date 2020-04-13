7b07f942e46137e3289ce5a9a3057d5f598b2e3f
William Vincent <will@wsvincent.com>
2020-04-13 12:07:00 -0400

    fix typos, add users migration file

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for djangox_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangox_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
