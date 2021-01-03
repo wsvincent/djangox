c35576508903124ac3acf634595ddfddcdcc89ed
Curtly Critchlow <curtlycritchlow@yahoo.com>
2021-01-03 08:04:32 -0400

    Update urls.py
    added necessary changes for production

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('website-web-management/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
