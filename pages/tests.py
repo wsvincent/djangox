61bd5187055121edf37ad2e8b2ea58b39cfbae09
Jorge Alvarado <alvaradosegurajorge@gmail.com>
2021-06-05 17:52:58 -0400

    Add basic tests to the pages app views

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from . import views
+
+
+class HomePageViewTests(TestCase):
+    def test_uses_correct_html(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, views.HomePageView.template_name)
+
+    def test_uses_correct_view(self):
+        response = resolve(reverse('home'))
+        self.assertEqual(
+            response.func.__name__, views.HomePageView.as_view().__name__
+        )
+
+
+class AboutPageViewTests(TestCase):
+    def test_uses_correct_html(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, views.AboutPageView.template_name)
+
+    def test_uses_correct_view(self):
+        response = resolve(reverse('about'))
+        self.assertEqual(
+            response.func.__name__, views.AboutPageView.as_view().__name__
+        )
