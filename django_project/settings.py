bdff5beea43abad09b6227fb4085f1dd39d74409
Roman Romanenko <roman@romanenko.com>
2024-10-26 19:08:05 +0000

    Removed URL.

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2
 DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", 'scds-ai.syndevops.com']
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
